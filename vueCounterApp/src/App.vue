fc9ad93c0349c7b0b659858e0b710c02dfdb67bd
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-12-28 22:49:52 +0100

    Vue counter

---
+++ vueCounterApp/src/App.vue
@@ -0,0 +1,33 @@
+
+<script setup>
+import {ref} from "vue";
+const count = ref(0);
+</script>
+<template>
+  <main>
+    <div class="">
+    <h4>The current count is ...</h4>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+    <button @click="count = 0"> Mettre à 0</button>
+  </div>
+  </main>
+
+
+</template>
+
+<style scoped>
+  main {
+background-color: aliceblue;
+height:100vh;
+widows:100vw ;
+display: flex;
+align-items: center;
+justify-content:center;
+
+  }
+  div {
+    text-align: center;
+  }
+</style>
