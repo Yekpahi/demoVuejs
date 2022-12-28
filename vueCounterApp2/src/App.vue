ca15aec4542678a6ceb7d934dfb3cc073efd2d8b
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-12-29 00:53:51 +0100

    counter 3 with vue js

---
+++ vueCounterApp2/src/App.vue
@@ -0,0 +1,43 @@
+
+<script setup>
+import { ref } from "vue"
+const count = ref(0);
+
+const addToCount = () => {
+  count.value = count.value + 1
+}
+
+const substracToCount = () => {
+  count.value = count.value - 1
+}
+
+const zeroToCount = () => {
+  count.value = 0
+}
+
+</script>
+<template>
+
+
+  <main>
+    <div>
+      <h4> The current count is ....</h4>
+      <h1> {{ count }}</h1>
+      <button @click="addToCount">+</button>
+      <button @click="zeroToCount">ZERO</button>
+      <button @click="substracToCount">-</button>
+     
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
