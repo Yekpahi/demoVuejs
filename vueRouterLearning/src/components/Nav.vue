0f7e75ebfa8b75e526b944f60daeaa41f0d11117
Yekpahi Prince <princeyekpahi@hotmail.fr>
2023-01-06 03:25:21 +0100

    Router

---
+++ vueRouterLearning/src/components/Nav.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+<RouterLink active-class="active" to="/">Home</RouterLink>
+<RouterLink active-class="active" to="/about">About</RouterLink>
+
+</template>
+
+<style>
+.active {
+    font-weight:900;
+    color: rgb(153, 104, 14)
+    }
+</style>
---
+++ vueRouterLearning/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+    <div>
+        <h2> About </h2>
+    </div>
+</template>
+
+<style scoped>
+</style>
