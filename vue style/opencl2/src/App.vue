b35ab08239b8ac057c9bf84d46913652269bafdf
Yekpahi Prince <princeyekpahi@hotmail.fr>
2023-01-24 14:31:43 +0100

    slot

---
+++ vue style/opencl2/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+import Comp1 from './components/comp1.vue';
+
+export default {
+  components: {
+    Comp1
+  },
+  data() {
+    return {
+      msg : ""
+    }
+  },
+  methods: {
+        // Définit la méthode utilisée par le payload pour mettre à jour la propriété data
+        setMessage(payload) {
+            this.msg = payload.message
+        }
+    }
+}
+</script>
+
+<template>
+
+
+
+  <div :class="$style.red">
+    <p :class="$style.white">Je suis blanc</p>
+  </div>
+  <p>{{ msg }}</p>
+  <Comp1 categorie="categorie" @custom-event-name="setMessage">
+   
+  </Comp1>
+<Comp1>
+  <template v-slot:slot1>Slot 1</template>
+</Comp1>
+</template>
+
+<style module>
+.red {
+  background-color: red;
+}
+
+.white {
+  color: aliceblue;
+}
+</style>
---
+++ vue style/opencl2/src/components/Comp1.vue
@@ -0,0 +1,14 @@
+<template>
+    <button @click="emitCustomEvent">Emit Event</button>
+    <slot name ="slot1">Les choses se passe bien</slot>
+</template>
+
+<script>
+export default {
+    methods: {
+        emitCustomEvent() {
+            this.$emit('custom-event-name', { message: 'Mon message personnalisé' })
+        }
+    }
+}
+</script>
